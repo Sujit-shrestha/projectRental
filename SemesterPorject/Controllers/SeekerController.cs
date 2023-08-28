24c89183994a20546641b6a108d06ee710175c8b
sujit <sujitshrestha0004@gmail.com>
2023-08-28 13:00:05 +0545

    Added somthing , idk what tho

---
+++ SemesterPorject/Controllers/SeekerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SemesterPorject.Data;
+using SemesterPorject.Models;
+
+namespace SemesterPorject.Controllers
+{
+    public class SeekerController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public SeekerController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<RentalSpaceModel> RentalSpaceList = _db.RentalSpace;
+
+            ViewBag.DisableFooter = true;
+            return View(RentalSpaceList);
+        }
+        
+    }
+}
