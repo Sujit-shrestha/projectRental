3acb0513ec079509200778325ecdf1d4cfcd8718
code002-ZeroTwo <siddharthpaudel41@gmail.com>
2023-08-30 22:07:05 +0545

    login and register user

---
+++ SemesterPorject/Areas/Identity/Data/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SemesterPorject.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AppUser class
+public class AppUser : IdentityUser
+{
+    [Column(TypeName ="nvarchar(100)")]
+    public String SeekerOrLister { get; set; }
+}
+
--- SemesterPorject/Program.cs
+++ SemesterPorject/Program.cs
@@ -1,13 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using SemesterPorject.Data;
+using SemesterPorject.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+// builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+  //  builder.Configuration.GetConnectionString("DefaultConnection")
+   // ));
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,11 +30,14 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
